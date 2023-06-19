9c01899972596636ad2112df41a4f63993424c5b
krlz <soy_solamente@hotmail.com>
2023-06-20 01:14:21 +0300

    add

--- src/pages/api/scrapper.js
+++ src/pages/api/scrapper.js
@@ -1,29 +1,17 @@
-import puppeteer from "puppeteer"
-import cheerio from "cheerio"
-import pLimit from "p-limit"
+import {load} from "cheerio";
+import puppeteerExtra from "puppeteer-extra";
+import blockResources from "puppeteer-extra-plugin-block-resources";
 
-let sites = [
-    "https://pb.proxybay.ca"
-];
-let notFoundSites = [];
-const fetchedSites = await fetchProxyUrls();
-console.log(fetchedSites)
-const cleanedSites = fetchedSites.map((site) => site.replace(/\/$/, '')); // Remove trailing slashes
+let site = "https://pb.proxybay.ca";
 
-export async function get({ request }) {
-    const limit = pLimit(5);
-    const allSites = sortSites([...sites, ...cleanedSites]);
-    const url = new URL(request.url)
-    const params = new URLSearchParams(url.search)
-    
-    let failedCount = 0;
-    for (const site of allSites) {
-        const someResults = await scrapeWebsite(site, params.get('q'));
-        if (someResults.length > 0) {
-            return {
-                body: JSON.stringify(someResults),
-            };
-        }
+export async function get({request}) {
+    const url = new URL(request.url);
+    const params = new URLSearchParams(url.search);
+    const someResults = await scrapeWebsite(site, params.get('q'));
+    if (someResults.length > 0) {
+        return {
+            body: JSON.stringify(someResults),
+        };
     }
 
     return {
@@ -34,9 +22,8 @@ export async function get({ request }) {
 async function scrapeWebsite(site, searchValue) {
     const results = [];
     try {
-        const browser = await puppeteer.launch({ headless: "new" });
+        const browser = await puppeteerExtra.launch({headless: "new"});
         const page = await browser.newPage();
-
         const endpoints = [
             `${site}/search.php?q=${searchValue}`,
             `${site}/s/?q=${searchValue}`,
@@ -44,10 +31,10 @@ async function scrapeWebsite(site, searchValue) {
         ];
 
         for (const endpoint of endpoints) {
-            await page.goto(endpoint, { timeout: 5000 }); 
+            await page.goto(endpoint, {timeout: 5000});
             const html = await page.content();
 
-            const $ = cheerio.load(html);
+            const $ = load(html);
             const links = $('a[href^="magnet:?"]');
 
             if (links.length > 0) {
@@ -60,13 +47,12 @@ async function scrapeWebsite(site, searchValue) {
                         const descriptionLink = parentList.find('a[href^="/description.php"]');
                         const descriptionText = descriptionLink.text().trim();
 
-                        results.push({ magnet: href, description: descriptionText });
+                        results.push({magnet: href, description: descriptionText});
                     }
                 });
                 return results;
-            }else{
+            } else {
                 console.log(`${endpoint} - No results found`);
-                notFoundSites.push(site);
             }
         }
         await page.close();
@@ -74,18 +60,17 @@ async function scrapeWebsite(site, searchValue) {
         return results;
     } catch (error) {
         console.error(`Error scraping ${site}:`, error.message);
-        notFoundSites.push(site);
         return results;
     }
 }
 
-function sortSites(sites) {
-    return sites.sort((a, b) => notFoundSites.filter(site => site === a).length - notFoundSites.filter(site => site === b).length);
-}
- 
 
-async function fetchProxyUrls() {
-    const response = await fetch('https://emulatorclub.com/the-pirate-bay-proxy-list/');
+/*
+* TODO: Scrape proxy urls from https://emulatorclub.com/the-pirate-bay-proxy-list
+* TODO: apply a heartbeat to the proxy urls to check if they are still alive
+*
+* async function fetchProxyUrls() {
+    const response = await fetch('https://emulatorclub.com/the-pirate-bay-proxy-list');
     const html = await response.text();
     const $ = cheerio.load(html);
 
@@ -99,3 +84,6 @@ async function fetchProxyUrls() {
 
     return urls;
 }
+*
+*
+* */
