f5053692822ea421867f7764cbc43f90245b2689
Carlos Rojas <c.rojas@portavita.eu>
2023-06-21 12:24:50 +0300

    add node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,11 @@
 import { defineConfig } from 'astro/config';
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
-    output: 'hybrid'
+  output: 'hybrid',
+  adapter: node({
+    mode: "standalone"
+  })
 });
